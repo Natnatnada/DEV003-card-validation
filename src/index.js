545505aeeb0d40b59f63611dc8df7a7661362687
NataliaAspee <n.aspeedelgado@gmail.com>
2022-12-05 23:15:22 -0300

    pruebasJS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import validator from './validator.js';
+/* id input numeroTarjeta
+id input botonComprobar*/
+let creditCardNumber= document.getElementById("cardNumber");
+let algoritmoLuhn= validator.isValid(creditCardNumber);
+document.getElementById("botonComprobar").addEventListener("click",(comprobarValidacion));
 
-console.log(validator);
+function comprobarValidacion (){
+    //llamada al inputhtml mediante el id para obtener los datos
+    let creditCardNumber= document.getElementById("cardNumber");
+    //funcion desde validator para comprobar la validez de la tarjeta alert para pruebas
+    
+    let algoritmoLuhn= validator.isValid(creditCardNumber);
+    if (algoritmoLuhn==true){
+       alert("tu compra funciono");
+    }
+else if(algoritmoLuhn==false){
+    alert("no");
+}
+}
+
+
+/*let creditCardNumber= document.getElementById("cardNumber")
+let algoritmoLuhn= validator.isValid(creditCardNumber);
+console.log(algoritmoLuhn);
+if (algoritmoLuhn ==true){
+    message = "Tu compra ha sido realizada con exito, Muchas gracias";
+
+}
+else if (algoritmoLuhn==false){
+    message = "Parece que tenemos un problema, vuelve a intentarlo";
+};
+*/
+
+/*añadir funcion al click, ahi se ejecuta el credit card number si se cumple*/
+
+document.getElementById("botonComprobar").addEventListener("click",(comprobarValidacion));
--- src/validator.js
+++ src/validator.js
@@ -1,11 +1,50 @@
 const validator = {
 
-  isValid: function () {
+  isValid(creditCardNumber) {
+    /*typeError: Cannot read property 'reverse' of undefined so creditCardNumber.split('').reverse(); ?
+    luego pasara a mi index algoritmoLuhn para hacer la comprobacion de la tarjeta
+    */
+    let value = creditCardNumber.reverse();
+    for(let i=0; i<value.length; i++){
+      if(i%2 !=0){
+        value[i] = value[i] * 2;
+        if(value[i] > 9){
+          value[i]= parseInt(string(value[i]).charAt(0))+parseInt(String(value[i]).charAt(1))
+
+        }
+      }
+    }
     
+    let sum = 0;
+
+    for(let i=1; i<value.length; i++){
+      sum += parseInt(value[i]);
+      
+    }
+    sum = sum * 9 % 10;
+    if (value[0] == sum){
+      return true
+    }
+    else{
+      return false
+    }
   }
 
-  
-  
+  /*
+maskify(creditCardNumber){
+  if(creditCardNumber.length> 4){
+    let hideNumber ="*".repeat(creditCardNumber.length -4);
+    let showLastFour =creditCardNumber.substring(creditCardNumber.length -4);
+    let mask =hideNumber+showLastFour;
+    return mask;
+  }
+  else {
+    return creditCardNumber;
+  }
+
+}
+    */
 };
+
 
 export default validator;
